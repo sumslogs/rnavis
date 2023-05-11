2899aec2a03be28358b07882be205a0ff605f377
Michael Souza <sumslogs@gmail.com>
2023-05-11 15:15:56 -0700

    RNArtist experiment

---
+++ structure_vis/rnartist/plot_2ds.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+setup_color() {
+  RED=$(printf '\033[31m\xe2\x9d\x8c')
+  GREEN=$(printf '\033[32m\xe2\x9c\x85')
+  YELLOW=$(printf '\033[33m\xe2\x9d\x97')
+  BLUE=$(printf '\033[34m')
+  BOLD=$(printf '\033[1m')
+  RESET=$(printf '\033[m')
+}
+
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+  echo ${RED}" $@"${RESET} >&2
+}
+
+message() {
+  echo ${GREEN}" $@"${RESET} >&2
+}
+
+step_to_do() {
+  echo ${YELLOW}"$@"${RESET} >&2
+}
+
+check_docker() {
+  command_exists docker || {
+    error "Docker not installed or not running"
+    exit 1
+  }
+
+  message "Docker installed."
+
+  result=$(docker ps)
+
+  if [[ -n "$result" ]]; then
+    message "Docker daemon is running."
+  else
+    error "It seems that the docker daemon is not running."
+    exit 1
+  fi
+
+  result=$(docker images -q fjossinet/rnartistcore)
+
+  if [[ -n "$result" ]]; then
+     message "Container fjossinet/rnartistcore installed."
+  else
+    step_to_do "Container fjossinet/rnartistcore not found. Installation launched..."
+    docker pull fjossinet/rnartistcore
+    result=$(docker images -q fjossinet/rnartistcore)
+    if [[ -n "$result" ]]; then
+       message "Container fjossinet/rnartistcore installed."
+    else
+      error "It seems that i was not able to install the container fjossinet/rnartistcore."
+      exit 1
+    fi
+  fi
+
+}
+
+main() {
+
+  setup_color
+
+  if [ $# -eq 0 ]; then
+
+      printf '\033[32m'
+
+      cat <<'EOF'
+This script parses and executes plotting instructions for one or several RNA secondary structures. These instructions have to be stored in a kts file. This script can also update your local copy of the plotting engine RNArtistCore.
+
+Usage: plot_2ds.sh [options] plotting_instructions_file.kts
+  Options:
+    -u  update your local copy of RNArtistCore
+
+EOF
+      exit 1
+  fi
+
+  case $1 in
+        -u)
+          printf "${YELLOW}Ready to update your local copy of RNArtistCore? [Y/n]${RESET}  "
+             read opt
+             case $opt in
+                n*|N*)  error "Aborted!"; exit 1;;
+             esac
+
+          check_docker
+
+          step_to_do "Before to update, some cleaning..."
+
+          for CONTAINER_ID in $(docker ps -a  | grep "fjossinet/rnartistcore" | awk '{printf("%s\n", $1)}')
+          do
+            step_to_do "stop & remove docker container $CONTAINER_ID"
+            docker stop $CONTAINER_ID
+            docker rm $CONTAINER_ID
+          done
+
+          for IMAGE_ID in $(docker images | grep "fjossinet/rnartistcore" | awk '{printf("%s\n", $3)}')
+          do
+            step_to_do "remove docker image $IMAGE_ID"
+            docker rmi $IMAGE_ID ;
+          done
+
+          docker pull fjossinet/rnartistcore ;
+          exit 0;;
+     esac
+
+  docker run -v "$PWD:/project" fjossinet/rnartistcore java -jar target/rnartistcore_with-dependencies.jar /project/$1
+}
+
+main "$@"
---
+++ structure_vis/rnartist/test.kts
@@ -0,0 +1,405 @@
+rnartist {
+  file = "/project/test.svg"
+  ss {
+    rna {
+      sequence = "GCGGAUUUAGCUCAGUUGGGAGAGCGCCAGACUGAAGAUCUGGAGGUCCUGUGUUCGAUCCACAGAAUUCGCACCA"
+    }
+    bracket_notation = "(((((((..((((........)))).(((((.......))))).....(((((.......))))))))))))...."
+  }
+  theme {
+    details_lvl = 5
+    
+    
+    color {
+        location = "1:1"
+        value = "#ecd1c3"
+    }
+    
+    color {
+        location = "2:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "3:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "4:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "5:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "6:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "7:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "8:1"
+        value = "#ebd3c6"
+    }
+    
+    color {
+        location = "9:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "10:1"
+        value = "#ebd3c6"
+    }
+    
+    color {
+        location = "11:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "12:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "13:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "14:1"
+        value = "#ecd2c4"
+    }
+    
+    color {
+        location = "15:1"
+        value = "#ebd3c6"
+    }
+    
+    color {
+        location = "16:1"
+        value = "#f2c7b2"
+    }
+    
+    color {
+        location = "17:1"
+        value = "#f5c0a7"
+    }
+    
+    color {
+        location = "18:1"
+        value = "#efcdbb"
+    }
+    
+    color {
+        location = "19:1"
+        value = "#e46e56"
+    }
+    
+    color {
+        location = "20:1"
+        value = "#f19072"
+    }
+    
+    color {
+        location = "21:1"
+        value = "#edd0c1"
+    }
+    
+    color {
+        location = "22:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "23:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "24:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "25:1"
+        value = "#3a4cc0"
+    }
+    
+    color {
+        location = "26:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "27:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "28:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "29:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "30:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "31:1"
+        value = "#ecd2c4"
+    }
+    
+    color {
+        location = "32:1"
+        value = "#f2c9b5"
+    }
+    
+    color {
+        location = "33:1"
+        value = "#e9795e"
+    }
+    
+    color {
+        location = "34:1"
+        value = "#b30326"
+    }
+    
+    color {
+        location = "35:1"
+        value = "#c93b37"
+    }
+    
+    color {
+        location = "36:1"
+        value = "#e26a53"
+    }
+    
+    color {
+        location = "37:1"
+        value = "#f6aa8c"
+    }
+    
+    color {
+        location = "38:1"
+        value = "#e06650"
+    }
+    
+    color {
+        location = "39:1"
+        value = "#efcebc"
+    }
+    
+    color {
+        location = "40:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "41:1"
+        value = "#ecd1c3"
+    }
+    
+    color {
+        location = "42:1"
+        value = "#ecd2c4"
+    }
+    
+    color {
+        location = "43:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "44:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "45:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "46:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "47:1"
+        value = "#edcfc0"
+    }
+    
+    color {
+        location = "48:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "49:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "50:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "51:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "52:1"
+        value = "#ebd3c6"
+    }
+    
+    color {
+        location = "53:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "54:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "55:1"
+        value = "#edd0c1"
+    }
+    
+    color {
+        location = "56:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "57:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "58:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "59:1"
+        value = "#ebd3c6"
+    }
+    
+    color {
+        location = "60:1"
+        value = "#ecd2c4"
+    }
+    
+    color {
+        location = "61:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "62:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "63:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "64:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "65:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "66:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "67:1"
+        value = "#ead3c7"
+    }
+    
+    color {
+        location = "68:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "69:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "70:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "71:1"
+        value = "#e9d4c9"
+    }
+    
+    color {
+        location = "72:1"
+        value = "#eecfbe"
+    }
+    
+    color {
+        location = "73:1"
+        value = "#f6ab8d"
+    }
+    
+    color {
+        location = "74:1"
+        value = "#f4c2aa"
+    }
+    
+    color {
+        location = "75:1"
+        value = "#f6b79c"
+    }
+    
+    color {
+        location = "76:1"
+        value = "#e8775d"
+    }
+    
+    
+    color {
+        type = "n"
+        value = "black"
+    }
+  }
+  layout {
+    junction {
+        type = 3
+        out_ids ="w e"
+    }
+  }
+}
